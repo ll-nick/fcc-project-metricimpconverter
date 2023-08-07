6fafffb0a3e1f73b205c790c3bc1a0e8d85d0ee6
ll-nick <github@le-large.net>
2023-08-07 12:39:11 +0200

    Implement convert handler functions

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,121 @@
+function findFirstLetterPosition(s) {
+  for (let i = 0; i < s.length; i++) {
+    if (s[i].match(/[a-zA-Z]/)) {
+      return i;
+    }
+  }
+  return -1;  // If no letters are found
+}
+
+function validUnit(unit) {
+  switch (unit) {
+    case 'gal':
+    case 'lbs':
+    case 'mi':
+    case 'L':
+    case 'kg':
+    case 'km':
+      return true;
+    default:
+      return false;
+  }
+}
+
+
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
+
+  this.getNum = function (input) {
+    let i = findFirstLetterPosition(input);
+
+    if (i === 0) return 1;
+
+    let numStr = input.substring(0, i);
+    if (numStr.match(/^\d+\.\d+$/) || numStr.match(/^\d+$/)) return parseFloat(numStr)
+    if (numStr.match(/^\d+[\/]\d+$/)) {
+      let values = numStr.split('/');
+      return parseFloat(values[0]) / parseFloat(values[1])
+    }
+
+    throw new Error('invalid number')
   };
 
-  this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
+  this.getUnit = function (input) {
+    let i = findFirstLetterPosition(input);
+    let numStr = input.substring(i);
+
+    if (validUnit(numStr)) return numStr
+
+    throw new Error('invalid unit')
   };
-  
-  this.convert = function(initNum, initUnit) {
+
+  this.getReturnUnit = function (initUnit) {
+    switch (initUnit) {
+      case 'gal':
+        return 'L';
+      case 'lbs':
+        return 'kg';
+      case 'mi':
+        return 'km';
+      case 'L':
+        return 'gal';
+      case 'kg':
+        return 'lbs';
+      case 'km':
+        return 'mi';
+      default:
+        throw new Error('Cannot convert ' + initUnit);
+    }
+  };
+
+  this.spellOutUnit = function (unit) {
+    switch (unit) {
+      case 'gal':
+        return 'gallons';
+      case 'lbs':
+        return 'pounds';
+      case 'mi':
+        return 'miles';
+      case 'L':
+        return 'liters';
+      case 'kg':
+        return 'kilograms';
+      case 'km':
+        return 'kilometers';
+      default:
+        throw new Error('Can\'t spell out unknown unit ' + unit)
+    }
+
+  };
+
+  this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    let result;
-    
+
+    switch (initUnit) {
+      case 'gal':
+        return initNum * galToL;
+      case 'lbs':
+        return initNum * lbsToKg;
+      case 'mi':
+        return initNum * miToKm;
+      case 'L':
+        return initNum / galToL;
+      case 'kg':
+        return initNum / lbsToKg;
+      case 'km':
+        return initNum / miToKm;
+      default:
+        console.error('Cannot convert ' + initUnit);
+    }
+
     return result;
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
+    return `${initNum} ${this.spellOutUnit(initUnit)} converts to ${returnNum.toFixed(5)} ${this.spellOutUnit(returnUnit)}`
   };
-  
+
 }
 
 module.exports = ConvertHandler;
